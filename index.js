a90e1d32dcfc797c6f9ee75942183ee30adc7a8e
Palash Mondal <palashm@live.in>
2023-04-24 17:07:43 +0530

    fix: lint issues

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function cloneArrayObjects(arrayInput = []) {
-  if (!Array.isArray(arrayInput)) {
-    throw new TypeError(`Expected an array, got ${typeof arrayInput}`);
-  }
+	if (!Array.isArray(arrayInput)) {
+		throw new TypeError(`Expected an array, got ${typeof arrayInput}`);
+	}
 
-  return arrayInput.map((object) => {
-    if (typeof object !== "object" || object === null) {
-      throw new TypeError(`Expected an object, got ${typeof object}`);
-    }
+	return arrayInput.map(object => {
+		if (typeof object !== 'object' || object === null) {
+			throw new TypeError(`Expected an object, got ${typeof object}`);
+		}
 
-    return { ...object };
-  });
+		return {...object};
+	});
 }
 
 export default cloneArrayObjects;
